447eb0be0e036c4e5c99e44b862896c86ecf1c8f
Miran <mirankavinda12@gmail.com>
2024-01-19 16:28:33 +0530

    #

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tile from './Tile';
+import Cell from './Cell';
+
+const Board = () => {
+  return (
+      <div>
+          Board
+    </div>
+  )
+}
+
+
+export default Board
