7df4df4bf408ad1903bc1dd8b9599590c67f4a54
Miran <mirankavinda12@gmail.com>
2024-01-20 17:21:17 +0530

    Refactor Board component and import statement

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import Tile from './Tile';
 import Cell from './Cell';
-import { Board } from '../../helper';
+import { Board } from '../helper';
 
 const BoardView = () => {
   const [board, setBoard] = useState(new Board());
 
-  return <div>{JOSN.stringify(board)}</div>;
+  const cells = board.cells.map((row, rowIndex) => {
+    return (
+      <div>
+        {row.map((col, colIndex) => {
+          return <Cell/>
+        })}
+      </div>
+    )
+  })
+
+  return <div>{cells}</div>;
 }
 
 
-export default Board
+export default BoardView
